4178fb681e0d188ab6089d6c1367a056a1ec0045
MiLKG4NG <devarshinichite@yahoo.com>
2023-01-17 15:51:14 +0530

    main file

---
+++ main/main/main.ino
@@ -0,0 +1,45 @@
+
+// Fill-in information from your Blynk Template here
+#define BLYNK_TEMPLATE_ID "TMPLe4f7fJUv"
+#define BLYNK_DEVICE_NAME "Plant Irrigation System"
+
+#define BLYNK_FIRMWARE_VERSION        "0.1.0"
+
+#define BLYNK_PRINT Serial
+//#define BLYNK_DEBUG
+
+#define APP_DEBUG
+
+// Uncomment your board, or configure a custom board in Settings.h
+//#define USE_SPARKFUN_BLYNK_BOARD
+//#define USE_NODE_MCU_BOARD
+//#define USE_WITTY_CLOUD_BOARD
+//#define USE_WEMOS_D1_MINI
+
+#include "BlynkEdgent.h"
+
+BLYNK_WRITE(V0)
+{
+  if(param.asInt()==0){
+  digitalWrite(2, HIGH);
+  }else{
+    digitalWrite(2, LOW);
+  }
+}
+
+BLYNK_CONNECTED(){
+  Blynk.syncVirtual(V0);
+}
+void setup()
+{
+  pinMode(2, OUTPUT);
+  Serial.begin(115200);
+  delay(100);
+
+  BlynkEdgent.begin();
+}
+
+void loop() {
+  BlynkEdgent.run();
+}
+
